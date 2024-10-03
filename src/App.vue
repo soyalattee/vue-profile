3e3128df6aeffffd8a0397f1b70b3f979baa726e
soyeon <uyyu1734@gmail.com>
2024-10-04 07:41:23 +0900

    first commit

--- src/App.vue
+++ src/App.vue
@@ -7,24 +7,14 @@
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+body {
+  margin: 0;
+  height: 100vh;
 }
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+#app {
+  padding: 0;
+  margin: 0;
+  height: 100%;
+  background: #00021e;
 }
 </style>
